98b478212d2953f65d978b03262bd63c19d7b016
alexandrasanchez-123 <alexamolina901@gmail.com>
2025-11-12 13:26:40 -0500

    Metrika avance

---
+++ 03-metrika/src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+import { TarjetasCards } from "./components/TarjetasCards";
+import { NavBar } from "./components/NavBar";
+
+export const App = () => {
+  return (
+    <>
+    <NavBar/>
+    <TarjetasCards/>
+    
+    </>
+  )}
+
